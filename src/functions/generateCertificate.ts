37b7b3364ad3b63f89e80ae0c67ccfa290c716ba
João Bispo <joaobispo2077@gmail.com>
2021-09-27 18:04:57 -0300

    feat: compile hbs certificate template to html filled with user vars

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,10 +1,32 @@
 import { document } from "src/utils/dynamodbClient";
+// import chromium from 'chrome-aws-lambda';
+
+import path from 'path';
+import fs from 'fs';
+import handlebars from 'handlebars';
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
 };
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string;
+}
+
+const compileTemplate = async (template: ITemplate) => {
+  const templatePath = path.join(process.cwd(), 'src', 'templates', 'certificate.hbs');
+
+  const html = fs.readFileSync(templatePath, 'utf8');
+  const compiledTemplate = handlebars.compile(html)(template);
+
+  return compiledTemplate;
+}
 
 export const handler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
@@ -18,6 +40,19 @@ export const handler = async (event) => {
       grade,
     },
   }).promise();
+
+  const medalPath = path.join(process.cwd(), 'src', 'templates', 'selo.png');
+  const medal = fs.readFileSync(medalPath, 'base64');
+
+  const templatePayload: ITemplate = {
+    id,
+    name,
+    grade,
+    date:   new Date().toLocaleDateString('pt-BR'),
+    medal,
+  };
+
+  const certificateHTML = await compileTemplate(templatePayload);
 
   return {
     statusCode: 201,
